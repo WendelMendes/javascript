8600184345af910c014d8233c48504aaef2dd42a
Wendel Mendes <wendel.omendes@gmail.com>
2022-07-06 14:06:25 -0300

    \

---
+++ Exemplos/ex7.js
@@ -0,0 +1,11 @@
+console.log("Olá mundo [F8]")
+//
+var velocidade = 70.1
+console.log(`A velocidade do seu carro é de ${velocidade}Km/h`)
+    if(velocidade>60){//Se
+        console.log("MULTADO!!! CANALHA")
+    }
+
+    else{//SENÃO
+        console.log("Mandou bem Irmão!")
+    }
