8ff431b361c1f4f8827723b7bbfcb9c02ecf7c5b
Wendel Mendes <wendel.omendes@gmail.com>
2022-07-16 10:54:55 -0300

    Update 1. tiposPrimitivos-contatenação.js

--- Exemplos/1. tiposPrimitivos-contatenação.js
+++ Exemplos/1. tiposPrimitivos-contatenação.js
@@ -8,6 +8,9 @@ console.log("Olá mundo [F8]")
     var n2 = Number(console.log("Digite outro número!"));
     var n3 = n1 + n2
 
+//Função para ver o tipo primitivo:
+    console.log(typeof(n3))
+
 //Contatenando com (+)
     console.log("A soma desses dois números é: " + n3 + " meu rapaz!") 
 //Contatenando com (circunflexo, $ e {})
