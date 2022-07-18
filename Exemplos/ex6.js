f16fef61ee2fa55c6d2ce7b0361454443bba65c7
Wendel Mendes <wendel.omendes@gmail.com>
2022-07-18 09:27:38 -0300

    :D

--- Exemplos/ex6.js
+++ Exemplos/ex6.js
@@ -1,9 +1,23 @@
-var velocidade = 70.1
-console.log(`A velocidade do seu carro é de ${velocidade}Km/h`)
-    if(velocidade>60){//Se
-        console.log("MULTADO!!! CANALHA")
+var velocidade = 60
+console.log(`A velocidade do seu carro é de ${velocidade}Km/h`);
+    if(velocidade>=60){//SE
+        console.log("MULTADO!!! CANALHA");
+    } else if (velocidade >= 21 && velocidade <= 59.9){
+        console.log('Mandando bem Irmão!');
+    } else{//SENÃO SE
+        console.log("Evite acidentes. Aumente a velocidade!");
     }
 
-    else{//SENÃO
-        console.log("Mandou bem Irmão!")
-    }
+var jogador1 = 0;
+var jogador2 = 0;
+var placar;
+switch (placar){
+    case placar = jogador1 > jogador2:
+        console.log('jogador 1 ganhou');
+        break;
+    case placar = jogador2 > jogador1:
+        console.log('jogador 2 ganhou')
+        break
+    default:
+        console.log('empate');
+}
---
+++ Exemplos/zArray-Object.js
@@ -0,0 +1,33 @@
+// como declarar um array
+/* let array = ['string', 1, true];
+console.log(array); */
+
+//pode guardar vários tipos de dados
+let array = ['string', 1, true, ['array1'],['array2']];
+/* console.log(array[3]); */
+
+/* // forEach
+array.forEach(function(item, index){console.log(item, index)}); */
+
+/* array.push([]);
+console.log(array); */
+
+/* array.pop();
+console.log(array); */
+
+/* array.shift();
+console.log(array); */
+
+/* array.unshift('novo item no início');
+console.log(array); */
+
+/* console.log(array.indexOf(true)); */
+
+/* array.splice(0, 3);
+console.log(array); */
+
+/* let novoArray = array.slice(0, 3);
+console.log(novoArray); */
+
+var object = {string: 'string', number: 1, boolean: true, array: ['array'], objectInterno: {objectInterno: 'objeto-interno'}};
+
