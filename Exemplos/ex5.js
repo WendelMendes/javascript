92c21ec637ad91fdf27c60d1e1dc5b55f305d8f8
Wendel Mendes <wendel.omendes@gmail.com>
2022-07-18 11:03:02 -0300

    :de:

--- Exemplos/ex5.js
+++ Exemplos/ex5.js
@@ -1,4 +1,18 @@
-pato = 1
-function batata() {
-pato = 2
+/* function nomeFunção(parametros){
+    bloco-instruções;
+} 
+nomeFunção();
+*/
+
+//criando função-declarativa
+function função(){
+    console.log('3 pó na mente');
 }
+//chamando função
+função();
+
+//criando função-curta (arrow-function)
+var funcao = () =>{
+    console.log('arrow-function');
+}
+funcao();
