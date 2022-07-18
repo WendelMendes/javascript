ce2b5d1ab0ee432f36c1e689fb8d16a1a1aefd3c
Wendel Mendes <wendel.omendes@gmail.com>
2022-07-18 10:00:31 -0300

    :D

---
+++ Exemplos/ex7.js
@@ -0,0 +1,24 @@
+/* for (expressão-inicial; condição; incremento){
+    declaração
+} */
+
+let array = ['valor1','valor1','valor2','valor3','valor4']
+let object = {propriedade1: 'valor1', propriedade2: 'valor2'}
+
+// for - executa uma instrução até que ela se torne falsa
+for (let indice = 0; indice<array.length; indice++){
+    console.log(indice);
+}
+/*------------------------------------------------------*/
+
+/* for (indice in objeto ou array){
+    declaração
+} */
+
+
+
+
+
+
+
+
---
+++ Exercicios/2. tabuada.js
@@ -0,0 +1,8 @@
+console.log('------------------------------------')
+n1=0
+n2=2
+for (var indexo = 0; indexo<=10 ;indexo++ ){
+    n3=n1*n2
+    console.log(`${n1} x ${n2} = ${n3}`)
+    n1++
+}
