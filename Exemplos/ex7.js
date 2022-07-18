461bcbd7b390b45b182f4bbf23cffc758d860093
Wendel Mendes <wendel.omendes@gmail.com>
2022-07-18 10:19:33 -0300

    Update ex7.js

--- Exemplos/ex7.js
+++ Exemplos/ex7.js
@@ -1,24 +1,38 @@
+let array = ['valor1','valor1','valor2','valor3','valor4']
+let object = {propriedade1: 'valor1', propriedade2: 'valor2'}
+
 /* for (expressão-inicial; condição; incremento){
     declaração
 } */
-
-let array = ['valor1','valor1','valor2','valor3','valor4']
-let object = {propriedade1: 'valor1', propriedade2: 'valor2'}
 
 // for - executa uma instrução até que ela se torne falsa
 for (let indice = 0; indice<array.length; indice++){
     console.log(indice);
 }
-/*------------------------------------------------------*/
+/*--------------------------*/
 
-/* for (indice in objeto ou array){
+/* for (indice in/of objeto/array){
     declaração
 } */
+// for/in - executa repetição de uma propriedade
+for ( i in object){
+    console.log(i);
+}
 
+// for/of - executa repetição de um valor
+for ( i of array){
+    console.log(i);
+}
+/*------------------------------------------------------*/
+//while - enquando
+var contagem = 100;
+while (contagem < 103){
+    contagem++
+    console.log(contagem)
+}
 
-
-
-
-
-
-
+//do while - faça enquanto (vai rodar pelo menos uma vez)
+do {
+    contagem++;
+    console.log(contagem);
+} while (contagem<106);
